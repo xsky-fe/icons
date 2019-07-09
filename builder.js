fb1c5d66bf97fd6af2140bd7231e9e5683f60fc6
王开朗 <wangkailang992@gmail.com>
2019-07-09 17:54:57 +0800

    remove old builder script

--- builder.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.readdir('./src/Icons', 'utf8', (err, data) => {
-  console.log(err, data);
-})
--- create-index.js
+++ create-index.js
@@ -1,16 +1,16 @@
-const fs = require ('fs');
+const fs = require('fs');
 const path = require('path');
-const {promisify} = require ('util');
+const { promisify } = require('util');
 
-const readdir = promisify (fs.readdir);
-const writeFile = promisify (fs.writeFile);
-const [source] = process.argv.slice (2);
+const readdir = promisify(fs.readdir);
+const writeFile = promisify(fs.writeFile);
+const [source] = process.argv.slice(2);
 
 (async () => {
-  const list = await readdir (source);
+  const list = await readdir(source);
   // fail in svgr index generator if icon name is index in any case
   const names = list
-    .filter(file => file.toLowerCase () !== 'index.tsx')
+    .filter(file => file.toLowerCase() !== 'index.tsx')
     .map (file => path.basename(file, '.tsx'));
   const exports = names.map(name => `import ${name} from './${name}';`);
   const content = exports.join('\n');
@@ -22,5 +22,5 @@ export enum ICONS {
 }
 `
   await writeFile(path.join (source, 'index.tsx'), interface+content+'\n'+exportDefault);
-}) ();
+})();
 
